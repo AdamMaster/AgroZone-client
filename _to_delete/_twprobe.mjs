04fd068ce1af74ad018a4ba58f0ddfda42f86bae
AdamMaster <lampezhev86@gmail.com>
2026-08-30 16:32:53 +0300

    комзи

--- _to_delete/_twprobe.mjs
+++
@@ -1,8 +0,0 @@
-import postcss from 'postcss'
-import tw from '@tailwindcss/postcss'
-import fs from 'fs'
-
-const css = fs.readFileSync('/tmp/twtest/in.css', 'utf8')
-const result = await postcss([tw()]).process(css, { from: '/tmp/twtest/in.css' })
-fs.writeFileSync('/tmp/twtest/out.css', result.css)
-console.log('DONE, bytes:', result.css.length)
